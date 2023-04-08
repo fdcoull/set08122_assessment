603350899d980f990f323922361ebe4923520a99
Finlay Daniel Coull <58175445+fdcoull@users.noreply.github.com>
2023-04-08 16:57:51 +0100

    Create grid

--- test/grid.c
+++ test/grid.c
@@ -1,19 +1,43 @@
 #include <stdio.h>
+
+void printGrid(char line[], int lineLength, int size)
+{
+		int i;
+		int j;
+		int grid[size][size];
+
+		for (i = 0; i < lineLength; i++)
+		{
+			for (j = 0; j < lineLength; j++)
+			{
+				if (i == 0 || i == lineLength - 1 || j == 0 || j == lineLength - 1)
+				{
+					line[j] = '#';
+				}
+				else if (i % 4 == 0 || j % 4 == 0)
+				{
+					line[j] = '#';
+				}
+				else
+				{
+					line[j] = ' ';
+				}
+			}
+			printf("%s\n", line);
+		}
+}
+
 int main(int argc, char **argv)
 {
 	int size;
-	int i;
+	int lineLength;
 
 	printf("Input grid size:\n");
 	scanf("%d", &size);
 
-	char line[(size * 2) + 1];
+	lineLength = (size + (3 * size)) + 1;
+	char line[lineLength];
 
-	for (i = 0; i < sizeof(line); i++)
-	{
-		line[i] = '#';
-	}
-
-	printf("%s\n", line);
+	printGrid(line, lineLength, size);
 
 }
